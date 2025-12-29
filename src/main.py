90cda090d20318bedb3a3e5aad575addd4dcdca5
Karun Kannan <karun.kannan15@gmail.com>
2025-12-29 08:51:28 -0500

    add cutoff logic to prevent spamming

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import requests as req
 import time
+from datetime import datetime, timedelta
 import os
 from twilio.rest import Client
 
@@ -12,16 +13,21 @@ TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
 TWILIO_AUTH = os.environ['TWILIO_AUTH']
 PHONE_NUMBER = os.environ['PHONE_NUMBER']
 
+last_sent_at = None
+
 def send_sms(client: Client):
     if client == None:
         print("Error: twilio client is uninitialized")
     message = client.messages.create(body="Book global reentry: %s" % SIGNUP_URL, from_="+12568261687", to=PHONE_NUMBER)
+    last_sent_at = datetime.now()
+    
 
 def check_appointments(client: Client) -> None:
     if client == None:
         print("Error: twilio client is uninitialized")
     appointments_response = req.get(REQ_URL % BOS_LOCATION_ID)
-    if len(appointments_response.json()) > 0:
+    send_message_cutoff = last_sent_at + timedelta(days=1)
+    if len(appointments_response.json()) > 0 and datetime.now() > send_message_cutoff:
         send_sms(client)
 
 def main() -> int:
